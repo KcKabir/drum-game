95b3ea633015afe90ee13145702c5ec27798d2e5
KcKabir <kckabir123@gmail.com>
2025-06-15 17:32:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var Number1 = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < Number1; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        
+        switch(this.innerHTML){
+            case "w":
+                var w = new Audio("tom-1.mp3");
+                w.play();
+            break;
+            case "a":
+                var a = new Audio("tom-2.mp3");
+                a.play();
+            break;
+            case "s":
+                var s = new Audio("tom-3.mp3");
+                s.play();
+            break;
+            case "d":
+                var d = new Audio("tom-4.mp3");
+                d.play();
+            break;
+            case "j":
+                var j = new Audio("snare.mp3");
+                j.play();
+            break;
+            case "k":
+                var k = new Audio("crash.mp3");
+                k.play();
+            break;
+            case "l":
+                var l = new Audio("kick-bass.mp3");
+                l.play();
+        }
+    });
+}
